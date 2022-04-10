f8aa8c133f78b850321470adce55061721f1522f
virtek <almasgafiew99@gmail.com>
2022-04-10 23:22:34 +0500

    init commit

---
+++ cogs/bank_talons.py
@@ -0,0 +1,35 @@
+"""
+Cog-file for talons
+"""
+import logging
+import disnake
+from disnake import ApplicationCommandInteraction
+from disnake.ext import commands
+
+import settings
+
+logger = logging.getLogger(__name__)
+
+
+class BankTalons(commands.Cog):
+    """
+    Cog for 4
+    """
+
+    def __init__(self, bot: disnake.ext.commands.Bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        """
+        Called when disnake bot object is ready
+        """
+
+        logger.info("%s Ready", __name__)
+
+
+def setup(client):
+    """
+    Disnake internal setup function
+    """
+    client.add_cog(BankTalons(client))
