8fb95abe3d2de1e822b389e68fb3ffee04508c92
howkawgew <zarbazan4ik@ya.ru>
2022-12-30 01:19:16 +0300

    feat: /rrs-sync command (owner only)

--- plasmotools/ext/reverse_role_sync/commands.py
+++ plasmotools/ext/reverse_role_sync/commands.py
@@ -16,6 +16,29 @@ class RRSCommands(commands.Cog):
         self.bot = bot
 
     @commands.slash_command(
+        name="rrs-sync"
+    )
+    @commands.is_owner()
+    async def rrs_sync_command(self, interaction: ApplicationCommandInteraction, user: disnake.User=None, user_id: str=None):
+        await interaction.response.defer(ephemeral=True)
+        if user_id is not None:
+            user = await self.bot.fetch_user(int(user_id))
+        if user is None:
+            await interaction.edit_original_message("Please provide a user")
+            return
+        rrs_core = self.bot.get_cog("RRSCore")
+        added_roles_ids, removed_roles_ids = await rrs_core.sync_user(user)
+        plasmo_guild = self.bot.get_guild(settings.PlasmoRPGuild.guild_id)
+        added_roles = [plasmo_guild.get_role(role_id) for role_id in added_roles_ids]
+        removed_roles = [plasmo_guild.get_role(role_id) for role_id in removed_roles_ids]
+        await interaction.edit_original_message(
+            f"Added roles: {', '.join([str(role) for role in added_roles])}\n"
+            f"Removed roles: {', '.join([str(role) for role in removed_roles])}\n"
+        )
+
+
+
+    @commands.slash_command(
         name="rrs-list",
         dm_permission=False,
     )
