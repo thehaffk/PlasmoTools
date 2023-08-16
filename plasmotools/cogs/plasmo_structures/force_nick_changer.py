ca8ba87d1f2566feac2d7e85d0a2e0c018c70275
Felix <haffk@duck.com>
2023-08-16 12:43:59 +0300

    feat: bot will sync every nickname in every structure on prod startup

--- plasmotools/cogs/plasmo_structures/force_nick_changer.py
+++ plasmotools/cogs/plasmo_structures/force_nick_changer.py
@@ -7,22 +7,41 @@ from plasmotools import models, settings
 
 logger = logging.getLogger(__name__)
 
-# todo: sync every structure on startup
-
 
 class ForceNickChanger(commands.Cog):
     def __init__(self, bot: disnake.ext.commands.Bot):
         self.bot = bot
+
+    async def _sync_plasmo_nickname(self, member: disnake.Member):
+        plasmo_guild = self.bot.get_guild(settings.PlasmoRPGuild.guild_id)
+        if plasmo_guild is None:
+            if settings.DEBUG:
+                return
+            logger.critical("Plasmo guild not found")
+            return
+
+        plasmo_member = plasmo_guild.get_member(member.id)
+        if plasmo_member is None:
+            return
+
+        if plasmo_member.display_name != member.display_name:
+            await member.edit(
+                nick=plasmo_member.display_name,
+                reason="Changing nicknames in structures is not allowed",
+            )
 
     @commands.Cog.listener("on_member_update")
     async def force_nick_changer(self, before: disnake.Member, after: disnake.Member):
         """
         Change user's nickname if it differs in structure and on Plasmo
         """
-        if before.nick == after.nick:
+        if (
+            after is None
+            or before.nick == after.nick
+            or before.guild.id == settings.PlasmoRPGuild.guild_id
+        ):
             return
-        if before.guild.id == settings.PlasmoRPGuild.guild_id:
-            return
+
         if not (
             await models.StructureGuild.objects.filter(
                 discord_id=before.guild.id
@@ -30,21 +49,7 @@ class ForceNickChanger(commands.Cog):
         ):
             return
 
-        plasmo_guild = self.bot.get_guild(settings.PlasmoRPGuild.guild_id)
-        if plasmo_guild is None:
-            if settings.DEBUG:
-                return
-            logger.critical("Plasmo guild not found")
-
-        plasmo_member = plasmo_guild.get_member(before.id)
-        if plasmo_member is None:
-            return
-
-        if plasmo_member.display_name != after.display_name:
-            await after.edit(
-                nick=plasmo_member.display_name,
-                reason="Changing nicknames in structures is not allowed",
-            )
+        await self._sync_plasmo_nickname(member=after)
 
     async def cog_load(self):
         """
@@ -52,6 +57,14 @@ class ForceNickChanger(commands.Cog):
         """
 
         logger.info("%s loaded", __name__)
+
+        if settings.DEBUG:
+            return
+        await self.bot.wait_until_ready()
+        logger.info("Running sync_plasmo_nickname on every player")
+        for guild in self.bot.guilds:
+            for member in guild.members:
+                await self._sync_plasmo_nickname(member=member)
 
 
 def setup(bot: disnake.ext.commands.Bot):
